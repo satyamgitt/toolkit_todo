0dd5cdabe72a723e9e8bebc4ed12e93041c405ad
satyamgitt <satyampriyam98@gmail.com>
2024-12-14 11:56:47 +0530

    feature2 added

--- src/store/Store.js
+++ src/store/Store.js
@@ -1,8 +1,9 @@
 const { configureStore } = require("@reduxjs/toolkit");
 
+import todoReducer from "../feature/todoslice.js"
 
 export default store = configureStore({
     reducer: {
-        todo: {}
+        todo: todoReducer
     }
 })
